6866889770e7edc42718f0d0fdadeb2eabd737a0
MithunKP <mthnkp@gmail.com>
2026-01-11 00:58:54 +0530

    authetication user detail inserting

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import authRoutes from "./routes/auth.routes";
 import protectedRoutes from "./routes/protected.routes";
+import userRoutes from "./routes/users.routes";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 // mount auth routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(3000, () => {
   console.log("API running at http://localhost:3000");
